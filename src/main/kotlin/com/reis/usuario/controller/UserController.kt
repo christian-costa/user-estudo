1b6e409aeda3c5af6cb571d6eb4e384e5ea7ba0d
Christian Reis <christian.reis.am@gmail.com>
2024-02-29 20:47:35 -0300

    Criação completa das operações camada controller

--- src/main/kotlin/com/reis/usuario/controller/UserController.kt
+++ src/main/kotlin/com/reis/usuario/controller/UserController.kt
@@ -3,7 +3,11 @@ package com.reis.usuario.controller
 import com.reis.usuario.dto.UserRequestDTO
 import com.reis.usuario.dto.UserResponseDTO
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -16,9 +20,34 @@ import java.time.LocalDateTime
 class UserController {
 
     @PostMapping
-    @ResponseStatus(HttpStatus.OK)
+    @ResponseStatus(HttpStatus.CREATED)
     fun createUser(@RequestBody userDTO: UserRequestDTO) : UserResponseDTO {
-        return UserResponseDTO(BigInteger.ONE, "fulano", "Fulano Da Silva", LocalDateTime.now(), listOf("Java", "Kotlin")) ;
+        return UserResponseDTO(BigInteger.ONE, "fulano", "Fulano Da Silva", LocalDateTime.now(), listOf("Java", "Kotlin"))
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun updateUser(@PathVariable id: BigInteger, @RequestBody userDTO: UserRequestDTO) : UserResponseDTO {
+        return UserResponseDTO(BigInteger.ONE, "fulano", "Fulano Da Silva", LocalDateTime.now(), listOf("Java", "Kotlin"))
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    fun findUserId(@PathVariable id: BigInteger) : UserResponseDTO {
+        return UserResponseDTO(BigInteger.ONE, "fulano", "Fulano Da Silva", LocalDateTime.now(), listOf("Java", "Kotlin"))
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    fun findUsers() : List<UserResponseDTO> {
+        return listOf(UserResponseDTO(BigInteger.ONE, "fulano", "Fulano Da Silva", LocalDateTime.now(), listOf("Java", "Kotlin")))
+    }
+
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun deleteUserId(@PathVariable id: BigInteger) {
+
     }
 
 }
